a9d429059b3e3bcddc4b5cbf96f0314bdf195a15
LucaRM95 <lucasrojas95@gmail.com>
2022-02-16 11:42:18 -0300

    Primera version de maquetado

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import './footer.css';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer container-fluid bg bg-dark">
+        <div className="row align-items-center justify-content-center">
+            <div className="col-md-6 ">
+                <p> &copy; 2022 Todos los derechos reservados </p>
+                <p>Política de Garantías | Libro de reclamos</p>
+            </div>
+            <div className="col-md-6 ">
+                <a href="">
+                    <i className="fab fa-instagram me-3 ms-3"></i>
+                </a>
+                <a href="">
+                    <i className="fab fa-facebook me-3 ms-3"></i>
+                </a>
+                <a href="">
+                    <i className="fab fa-twitter me-3 ms-3"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
