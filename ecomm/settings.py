d7520de8efffe02a2d989ea9f69c260771a6d076
maegop <megordon@alu.ucam.edu>
2022-02-25 17:23:21 +0100

    settings changes

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -112,7 +112,6 @@ if DEBUG:
     DATABASES["default"]={
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-
     }
 else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
