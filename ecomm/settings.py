f945425c394247fe1b9fd814ccf6b8cd5c3116d4
maegop <megordon@alu.ucam.edu>
2022-03-03 15:30:35 +0100

    update

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -34,9 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #admin teplate
     'jazzmin',
-    #django defaut apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
